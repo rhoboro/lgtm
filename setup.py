b24fcd1d3120f0e13270bd0381d4e79faf7f6872
rhoboro <rhoboro@gmail.com>
2019-12-28 16:01:47 +0900

    setup.pyの作成

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='lgtm',
+    version='1.0.0',
+    packages=find_packages(exclude=('tests',)),
+    install_requires=[
+        'Click~=7.0',
+        'Pillow~=6.2.0',
+        'requests~=2.22.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lgtm=lgtm.core:cli'
+        ]
+    }
+)
