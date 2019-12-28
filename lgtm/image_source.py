b3e466531c79f5fcced56c00e9901adfaf1f7cc5
rhoboro <rhoboro@gmail.com>
2019-12-28 14:17:55 +0900

    URLから画像を取得するクラスの実装

--- lgtm/image_source.py
+++ lgtm/image_source.py
@@ -1,3 +1,8 @@
+from io import BytesIO
+
+import requests
+
+
 class LocalImage:
     """ファイルから画像を取得する"""
 
@@ -6,3 +11,15 @@ class LocalImage:
 
     def get_image(self):
         return open(self._path, "rb")
+
+
+class RemoteImage:
+    """URLから画像を取得する"""
+
+    def __init__(self, path):
+        self._url = path
+
+    def get_image(self):
+        data = requests.get(self._url)
+        # バイトデータをファイルオブジェクトに変換
+        return BytesIO(data.content)
--- tests/test_image_source.py
+++ tests/test_image_source.py
@@ -12,3 +12,17 @@ class LocalImageTest(unittest.TestCase):
         with open(path, "rb") as f:
             expected = f.read()
         self.assertEqual(expected, actual)
+
+
+class RemoteImageTest(unittest.TestCase):
+    def test_get_image(self):
+        from lgtm.image_source import RemoteImage
+
+        url = "https://raw.githubusercontent.com/rhoboro/lgtm/master/tests/data/test_image.jpg"
+        with RemoteImage(url).get_image() as f:
+            actual = f.read()
+
+        path = os.path.dirname(__file__) + "/data/test_image.jpg"
+        with open(path, "rb") as f:
+            expected = f.read()
+        self.assertEqual(expected, actual)
