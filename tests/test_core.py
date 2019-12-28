9676564832cc9699c36b3a3e44e36fb3847ce637
rhoboro <rhoboro@gmail.com>
2019-12-28 16:01:23 +0900

    各処理の呼び出しを実装

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,25 @@
+import os
 import unittest
+from pathlib import Path
+
+from lgtm.drawer import OUTPUT_NAME
 
 
 class LgtmTest(unittest.TestCase):
+    def setUP(self):
+        output_path = Path(OUTPUT_NAME)
+        if output_path.exists():
+            output_path.unlink()
+
+    def tearDown(self):
+        output_path = Path(OUTPUT_NAME)
+        output_path.unlink()
+
     def test_lgtm(self):
         from lgtm.core import lgtm
 
-        self.assertIsNone(lgtm('./python.jpeg', 'LGTM'))
+        path = os.path.dirname(__file__) + "/data/test_image.jpg"
+        lgtm(path, "dog")
+
+        output_path = Path(OUTPUT_NAME)
+        self.assertTrue(output_path.exists())
